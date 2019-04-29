11aded9b010b18d5e13aa36e2e0aff5bfb5f06ee
dburdyga <dariaburdyga@gmail.com>
2019-04-29 10:28:34 +0300

    router

---
+++ src/router/RouteNames.ts
@@ -0,0 +1,8 @@
+export enum RouteNames {
+    Cards = 'Cards',
+    Tasks = 'Tasks',
+    Login = 'Login',
+    Register = 'Register',
+    Account = 'Account',
+    Card = 'Card',
+}
---
+++ src/router/index.ts
@@ -0,0 +1,92 @@
+import Vue from 'vue';
+import Router from 'vue-router';
+import Cards from '../views/Cards.vue';
+import Tasks from '../views/Tasks.vue';
+import Login from '../views/Login.vue';
+import Register from '../views/Register.vue';
+import Card from '../views/Card.vue';
+import PageNotFound from '../views/PageNotFound.vue';
+import UserAccount from '../views/UserAccount.vue';
+import {RouteNames} from '@/router/RouteNames';
+import {AuthService} from '@/common/services/AuthService';
+
+Vue.use(Router);
+
+const router = new Router({
+    mode: 'history',
+    linkActiveClass: 'is-active',
+    linkExactActiveClass: 'is-exact-active',
+    routes: [
+        {
+            path: '/cards/:cardId',
+            component: Card,
+            props: true,
+            name: RouteNames.Card,
+       },
+        {
+            path: '/',
+            component: Cards,
+            name: RouteNames.Cards,
+        },
+        {
+            path: '/account',
+            component: UserAccount,
+            name: RouteNames.Account,
+            meta: {
+                requiresAuth: true,
+            },
+        },
+        {
+            path: '/login',
+            component: Login,
+            name: RouteNames.Login,
+            meta: {
+                requiresGuest: true,
+            },
+        },
+        {
+            path: '/register',
+            component: Register,
+            name: RouteNames.Register,
+            meta: {
+                requiresGuest: true,
+            },
+        },
+        {
+            path: '/home',
+            redirect: {name: RouteNames.Cards},
+        },
+        {
+            path: '/tasks',
+            component: Tasks,
+            name: RouteNames.Tasks,
+            meta: {
+                requiresAuth: true,
+            },
+        },
+        {
+            path: '*',
+            component: PageNotFound,
+        },
+    ],
+    scrollBehavior(to, from, savedPosition) {
+        return savedPosition || {x: 0, y: 0};
+    },
+});
+
+router.beforeEach((to, from, next) => {
+    const requiresAuth = to.matched.some((route) => route.meta.requiresAuth);
+    const requiresGuest = to.matched.some((route) => route.meta.requiresGuest);
+    if (requiresAuth && !AuthService.isLoggedIn()) {
+        next({
+            name: RouteNames.Login,
+            query: {redirect: to.fullPath },
+        });
+    } else if (requiresGuest && AuthService.isLoggedIn()) {
+        next({name: RouteNames.Cards});
+    } else {
+        next();
+    }
+});
+
+export default router;
