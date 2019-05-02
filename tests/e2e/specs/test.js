2f9aa6bdbcc5faa9a7a1f332a93afaffc0a9a286
Daria <dariaburdyga@gmail.com>
2019-05-02 22:00:43 +0300

    Test for Registration

--- tests/e2e/specs/test.js
+++ tests/e2e/specs/test.js
@@ -2,7 +2,7 @@
 // http://nightwatchjs.org/guide#usage
 
 module.exports = {
-  'test for Login': browser => {
+  'Test for Login': browser => {
     browser
         .url(process.env.VUE_DEV_SERVER_URL)
         .waitForElementVisible('#app', 5000)
@@ -11,6 +11,17 @@ module.exports = {
         .assert.visible('#password.input')
         .assert.elementPresent('.button.is-primary')
         .assert.containsText('a', 'Registration')
+        .end()
+  },
+  'Test for Registration': browser => {
+    browser
+        .url('http://localhost:8080/register')
+        .waitForElementVisible('#app', 5000)
+        .assert.elementPresent('.register-form')
+        .assert.visible('#email.input')
+        .assert.visible('#password.input')
+        .assert.visible('#passwordConfirm.input')
+        .assert.elementPresent('.button.is-primary')
         .end()
   }
 };
