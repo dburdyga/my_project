a83c2a4c56e67469f0ce23a53eaa5d4278c2b21d
Daria <dariaburdyga@gmail.com>
2019-05-02 23:34:02 +0300

    Test for Login and Cards Page

--- tests/e2e/specs/test.js
+++ tests/e2e/specs/test.js
@@ -2,15 +2,20 @@
 // http://nightwatchjs.org/guide#usage
 
 module.exports = {
-  'Test for Login': browser => {
+  'Test for Login and Cards Page': browser => {
     browser
         .url(process.env.VUE_DEV_SERVER_URL)
         .waitForElementVisible('#app', 5000)
         .assert.elementPresent('.login-form')
-        .assert.visible('#email.input')
-        .assert.visible('#password.input')
-        .assert.elementPresent('.button.is-primary')
-        .assert.containsText('a', 'Registration')
+        .setValue('input[type=text]', 'test@test.com')
+        .setValue('input[type=password]', 'test1234')
+        .click('.button.is-primary')
+        .pause(5000)
+        .waitForElementVisible('.tasks', 5000)
+        .assert.containsText('h1', 'Requirements')
+        .click('.button.is-primary')
+        .pause(5000)
+        .assert.visible('.table-container')
         .end()
   },
   'Test for Registration': browser => {
